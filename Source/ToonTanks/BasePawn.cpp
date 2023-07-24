8dfb842c522ee8ee30cae78536fb915a8f1c497c
AndreD4 <andremai629@gmail.com>
2023-07-24 11:24:50 +0900

    Constructing the Capsule

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -2,6 +2,7 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -9,14 +10,18 @@ ABasePawn::ABasePawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+  CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+  RootComponent = CapsuleComp;
 }
 
 // Called when the game starts or when spawned
 void ABasePawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
+
+
 
 // Called every frame
 void ABasePawn::Tick(float DeltaTime)
