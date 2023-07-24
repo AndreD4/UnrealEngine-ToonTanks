a5f2472592c0265f68ca9bd704a2cfb659bbbb04
AndreD4 <andremai629@gmail.com>
2023-07-24 11:43:24 +0900

    StaticMeshComponent

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -9,25 +9,30 @@
 UCLASS()
 class TOONTANKS_API ABasePawn : public APawn
 {
-	GENERATED_BODY()
+  GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	ABasePawn();
+  // Sets default values for this pawn's properties
+  ABasePawn();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
 
 private:
-    UPROPERTY()
-   class  UCapsuleComponent* CapsuleComp;
- 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+  UPROPERTY()
+  class UCapsuleComponent *CapsuleComp;
+  UPROPERTY()
+  UStaticMeshComponent *BaseMesh;
+  UPROPERTY()
+  UStaticMeshComponent *TurretMesh;
+  UPROPERTY()
+  USceneComponent* ProjectileSpawnPoint;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+public:
+  // Called every frame
+  virtual void Tick(float DeltaTime) override;
 
+  // Called to bind functionality to input
+  virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 };
