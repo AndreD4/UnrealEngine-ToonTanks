99b95b2d3a62d2b8dad157f8b6eeb1c1f861251e
AndreD4 <andremai629@gmail.com>
2023-08-01 18:52:26 +0900

    Interpolating target rotation

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -3,6 +3,8 @@
 
 #include "BasePawn.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values 
 ABasePawn::ABasePawn()
@@ -27,7 +29,14 @@ void ABasePawn::RotateTurret(FVector LookAtTarget)
 {
     FVector ToTarget = LookAtTarget - TurretMesh->GetComponentLocation();
     FRotator LookAtRotation = FRotator(0.f, ToTarget.Rotation().Yaw, 0.f);
-    TurretMesh->SetWorldRotation(LookAtRotation);
+
+    TurretMesh->SetWorldRotation(
+       FMath::RInterpTo(
+       TurretMesh->GetComponentRotation(),
+       LookAtRotation, 
+       UGameplayStatics::GetWorldDeltaSeconds(this),
+       5.f)
+    );
 }
 
 
